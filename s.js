7f95f21402f62f2b486cf001eb28626ebdd3b08a
RAYMOND WILFRED <raymondwilfredw@gmail.com>
2026-01-21 02:04:10 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("registerForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const phone = document.getElementById("phone").value.trim();
+
+       
+        if (name === "" || email === "" || phone === "") {
+            alert("Please fill all fields");
+            return;
+        }
+
+        const userData = {
+            name: name,
+            email: email,
+            phone: phone
+        };
+
+        console.log("Registered User:", userData);
+
+       
+        document.getElementById("form-status").textContent =
+            `Thank you, ${name}! Registration successful.`;
+
+        // Reset form
+        form.reset();
+    });
+
+});
